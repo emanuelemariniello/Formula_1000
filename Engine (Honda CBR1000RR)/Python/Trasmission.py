228b3403cdec928076166b00588bb06249a18f7d
emanuelemariniello <mariniello.emanuele@gmail.com>
2020-07-15 11:43:00 +0200

    First commit

--- Engine (Honda CBR1000RR)/Python/Trasmission.py
+++ Engine (Honda CBR1000RR)/Python/Trasmission.py
@@ -1,5 +1,5 @@
 r = 330.2  # Rolling radius wheels
-mu = 1.5  # Coefficient of friction
+mu = 1.2  # Coefficient of friction
 hm = 325  # Height centre of mass in mm
 L = 3250  # Wheelbase in mm
 m = 490  # weight of the Formula 1000 in Kg
@@ -8,8 +8,8 @@ Wr = W * (L * 0.55) / L  # Static rear axle load in N (main part of the weight o
 Wf = W * (L * 0.45) / L  # Static front axle load in N
 F = (Wr * mu) / (1 - (hm * mu / L))  # Traction Force in N
 dWx = (F * hm) / L  # +- Longitudinal load transfer in N
-WR = (Wr + dWx) / 2  # Rear load of each wheel
-WF = (Wf - dWx) / 2  # Front load of each wheel
+WR = (Wr + dWx) / 2  # Rear load of each wheel in N
+WF = (Wf - dWx) / 2  # Front load of each wheel in N
 
 Twheels = round((WR * 2 * r * mu) / 1000)  # Torque through transmission required at least in Nm
 a = F / W  # Acceleration in g
